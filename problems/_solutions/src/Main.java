374f1c3bf9e0d711bfcfc23d37863d48724aa06f
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-09-07 20:54:39 +0530

    Remove files & images

--- problems/_solutions/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // tic_tac_toe.TicTacToeTest.main(args);
-    }
-}
--- problems/snake-and-ladder/README.md
+++
@@ -1,3 +0,0 @@
-## Snake and Ladder
-
-[Click here](/problems/_solutions/src/main/java/com/solution/snake_and_ladder/) to check the solution.
